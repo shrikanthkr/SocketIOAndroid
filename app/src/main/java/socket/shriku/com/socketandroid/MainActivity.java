e54a5c7e0724069551f3837e2980fc55cf2f7e81
shrikanth_kr <shrikanth_kr@yahoo.com>
2015-03-21 22:46:41 +0530

    initial

---
+++ app/src/main/java/socket/shriku/com/socketandroid/MainActivity.java
@@ -0,0 +1,52 @@
+package socket.shriku.com.socketandroid;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://192.168.56.1:3000");
+        } catch (URISyntaxException e) {}
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSocket.connect();
+        mSocket.emit("event","{key: \"value\"}");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
